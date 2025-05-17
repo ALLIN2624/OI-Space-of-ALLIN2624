6bcd92e4c5828b5c3596e933c6583b20ebd6fbfb
ALLIN2624 <allin2624@outlook.com>
2025-05-17 17:57:08 +0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -20,16 +20,18 @@ namespace ALLIN2624 {
 		istream &inner;
 		ostream &outter;
 		IO_t(istream &_inner = cin, ostream &_outter = cout): inner(_inner), outter(_outter)
-			{ ios::sync_with_stdio(0); cin.tie(0), cout.tie(0); }
+			{ ios::sync_with_stdio(0); inner.tie(0), outter.tie(0); }
+		void sp() { outter << " "; }
+		void ln() { outter << "\n"; }
 		template <typename Tp> void in(Tp &x) { inner >> x; }
 		template <typename Tp> void sp(Tp &x) { outter << x << " "; }
 		template <typename Tp> void ln(Tp &x) { outter << x << "\n"; }
 		template <typename Tp> void in(art<Tp> &a) { for (auto &u : a) inner >> u; }
 		template <typename Tp> void sp(art<Tp> &a) { for (auto &u : a) outter << u << " "; }
 		template <typename Tp> void ln(art<Tp> &a) { for (auto &u : a) outter << u << "\n"; }
-		template <typename Tp, typename... Args> void in(Tp &x, Args... args) { in(x), in(args...); }
-		template <typename Tp, typename... Args> void sp(Tp &x, Args... args) { sp(x), sp(args...); }
-		template <typename Tp, typename... Args> void ln(Tp &x, Args... args) { ln(x), ln(args...); }
+		template <typename Tp, typename... Args> void in(Tp &x, Args& ...args) { in(x), in(args...); }
+		template <typename Tp, typename... Args> void sp(Tp &x, Args& ...args) { sp(x), sp(args...); }
+		template <typename Tp, typename... Args> void ln(Tp &x, Args& ...args) { ln(x), ln(args...); }
 	} IO, dbg(cin, cerr);
 	
 	void solve() {
