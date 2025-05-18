4fa9532a9c47765747be1a82d3d447c84aed27c2
ALLIN2624 <allin2624@outlook.com>
2025-05-18 21:04:09 +0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -14,24 +14,31 @@ namespace ALLIN2624 {
 	using i64 = int64_t;
 	using u32 = uint32_t;
 	using u64 = uint64_t;
+#if defined(LOCAL) || defined(linux)
+	using i128 = __int128_t;
+	using u128 = __uint128_t;
+#else
+	using i128 = int64_t;
+	using u128 = uint64_t;
+#endif
 	template <typename Tp> using art = vector<Tp>;
 	
 	struct IO_t {
 		istream &inner;
 		ostream &outter;
-		IO_t(istream &_inner = cin, ostream &_outter = cout): inner(_inner), outter(_outter)
-			{ ios::sync_with_stdio(0); inner.tie(0), outter.tie(0); }
+		IO_t(istream &_inner = cin, ostream &_outter = cout, i32 pos = 10): inner(_inner), outter(_outter)
+			{ ios::sync_with_stdio(0); inner.tie(0), outter.tie(0), outter << setprecision(pos); }
 		void sp() { outter << " "; }
 		void ln() { outter << "\n"; }
 		template <typename Tp> void in(Tp &x) { inner >> x; }
-		template <typename Tp> void sp(Tp &x) { outter << x << " "; }
-		template <typename Tp> void ln(Tp &x) { outter << x << "\n"; }
+		template <typename Tp> void sp(Tp x) { outter << x << " "; }
+		template <typename Tp> void ln(Tp x) { outter << x << "\n"; }
 		template <typename Tp> void in(art<Tp> &a) { for (auto &u : a) inner >> u; }
 		template <typename Tp> void sp(art<Tp> &a) { for (auto &u : a) outter << u << " "; }
 		template <typename Tp> void ln(art<Tp> &a) { for (auto &u : a) outter << u << "\n"; }
 		template <typename Tp, typename... Args> void in(Tp &x, Args& ...args) { in(x), in(args...); }
-		template <typename Tp, typename... Args> void sp(Tp &x, Args& ...args) { sp(x), sp(args...); }
-		template <typename Tp, typename... Args> void ln(Tp &x, Args& ...args) { ln(x), ln(args...); }
+		template <typename Tp, typename... Args> void sp(Tp x, Args... args) { sp(x), sp(args...); }
+		template <typename Tp, typename... Args> void ln(Tp x, Args... args) { ln(x), ln(args...); }
 	} IO, dbg(cin, cerr);
 	
 	void solve() {
